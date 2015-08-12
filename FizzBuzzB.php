df1f1e5afb960f3b170a91098f2dd1caad7681c9
Jamie Doris <jamie.doris@ubm.com>
2015-08-12 14:54:56 -0400

    cleanup, removed redundant comments

--- FizzBuzzB.php
+++ FizzBuzzB.php
@@ -1,14 +1,10 @@
 <?php
 /**
  * FizzBuzz w/ Recursion
- * Cliche, but useful.  Prints numbers from 1 to 100. For multiples of
- * three print "Fizz" instead of the number and for the multiples of five print
- * "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
- *
- * This time with recursion!
+ * Jamie Doris
  */
 
-function fizzBuzz($num = 0) {
+function fizzBuzz($num) {
   if ($num < 101) {
     $nextnum = $num+1;
     if ($num % 3 == 0 && $num % 5 == 0) { // divisible by both 3 and 5
